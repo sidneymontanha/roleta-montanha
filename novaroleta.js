97fd9d3ceac3f7da3c0a1ea94df182c6e71be919
sidnei paes de camargo <94030294+sidneymontanha@users.noreply.github.com>
2024-08-20 05:33:39 -0300

    novocomite
    auteracao para ksop

--- novaroleta.js
+++ novaroleta.js
@@ -1,120 +1,238 @@
-const roleta = document.querySelector('img#roleta')
-const wheel = document.querySelector('audio')
-const inpt = document.querySelector('.valor')
-const res = document.querySelector('.RES')
-const video= document.querySelector('.video')
-const sold = document.querySelector('#valor')
-const acumulado = document.querySelector('#acumolo')
+const roleta = document.querySelector('img.roleta');
+const wheel = document.querySelector('audio');
+const inputValor = document.querySelector('.valor');
+const resultado = document.querySelector('.RES');
+const video = document.querySelector('.video');
+const video1 = document.querySelector('.video1');
+const acumuladoInput = document.querySelector('#acumolo');
+const sorteioSection = document.querySelector('.sorteio');
+const premiacaoSection = document.querySelector('.premiacao');
+const abridor0 = document.querySelector('.abridor0')
+const abridor1= document.querySelector('.abridor1')
+const bone0 = document.querySelector('.bone0')
+const bone1 = document.querySelector('.bone1')
+const bone2 = document.querySelector('.bone2')
+const bone3 = document.querySelector('.bone3')
+const bone4 = document.querySelector('.bone4')
+const camiseta0 = document.querySelector('.camiseta0')
+const camiseta1 = document.querySelector('.camiseta1')
+const camiseta2 = document.querySelector('.camiseta2')
+const caneca = document.querySelector('.caneca')
+const chinelo1 = document.querySelector('.chinelo1')
+const chinelo2 = document.querySelector('.chinelo2')
+const mousepad = document.querySelector('.mousepad')
+const patch0 = document.querySelector('.patch0')
+const patch1 = document.querySelector('.patch1')
+const patch2 = document.querySelector('.patch2')
+const sacochila = document.querySelector('.sacochila')
+const suporte = document.querySelector('.suporte')
+const vara = document.querySelector('.vara')
 
-roleta.addEventListener('click', girar)
 
-const videoplay = () =>{
-    video.style.display = 'block'
-    video.style.width = '100Vw'
-    video.style.height = '100Vh'
-    video.play()
-    setTimeout(() =>{
-    video.style.display ='none'
-    },7000)
+
+roleta.addEventListener('click', girar);
+
+const mostreSorteio = ()=>{
+    sorteioSection.style.display = 'block'
+
+}
+
+const ocultaSorteio = ()=>{
+    sorteioSection.style.display = 'none'
+    
+}
+
+function mostrepremio(premioelemento){
+    premioelemento.style.display = 'block'
+}
+
+function ocultarpremio(premioelemento){
+    premioelemento.style.display = 'none'
 }
 
 
-function girar(){
 
-    if (sold.value < 1){
-        res.innerHTML = 'não tem um valor de aposta, favor colocar credito'
-    }else{
+function playVideo(videoElement, y) {
+    ocultaSorteio()
+    mostrepremio(y)
+    vara.style.display = 'none'
+    videoElement.style.display = 'block';
+    videoElement.style.width = '100vw';
+    videoElement.style.height = '100vh';
+    videoElement.play();
+    
 
-        let acum = Number(acumulado.value)
-        acum = acum + 1
-        //console.log(acum)
-        acumulado.value = acum
-        let rand = Math.random() * 7200
-        //console.log(rand)
-        calcular(rand)
-        function calcular(rand){
-            res.innerHTML = ''
-            valor = rand/360
-            valor = (valor - parseInt(valor.toString().split('.')[0])) * 360
-            //console.log(valor)
- 
-            roleta.style.transform = 'rotate('+ rand+'deg)'
-            
-                wheel.play()
+    setTimeout(() => {
+        ocultarpremio(y)
+        videoElement.style.display = 'none';
+        mostreSorteio()
+        vara.style.display = 'block'
+    }, 7000);
+}
+
+function girar() {
+    /*if (inputValor.value < 1) {
+        resultado.innerHTML = 'Não tem um valor de aposta, favor colocar crédito';
+        return;
+    }*/
+
+    let acumulado = Number(acumuladoInput.value);
+    acumulado += 1;
+    acumuladoInput.value = acumulado;
+
+    const rotacaoAleatoria = Math.random() * 7200;
+    calcularRotacao(rotacaoAleatoria);
+
+    //inputValor.value = inputValor.value > 1 ? inputValor.value - 1 : '';
+}
+
+function calcularRotacao(rotacaoAleatoria) {
+    resultado.innerHTML = '';
+    const valorRotacao = rotacaoAleatoria % 360;
+
+    roleta.style.transform = `rotate(${rotacaoAleatoria}deg)`;
+    wheel.play();
+
+    setTimeout(() => {
+        verificarPremio(valorRotacao);
+    }, 15500);
+}
+
+function verificarPremio(angulo) {
+    let premio = '';
+    let limiteMaximo = 0;
+    let premioelemento = null
+    
+    if (angulo > 10.7  && angulo <= 31) {
+        premio = 'camiseta';
+        limiteMaximo = 10;
+        premioelemento = camiseta0;
+    } else if (angulo > 31 && angulo <= 48.5) {
+        premio = 'boné';
+        limiteMaximo = 32;
+        premioelemento = bone0;
+    } else if (angulo > 48.5 && angulo <= 66.7) {
+        premio = 'sacochila';
+        limiteMaximo = 74;
+        premioelemento = sacochila;
+    } else if (angulo > 66.7 && angulo <= 82.2) {
+        premio = 'patch';
+        limiteMaximo = 95;
+        premioelemento = patch0;
+    } else if (angulo > 82.2 && angulo <= 101) {
+        premio = 'caneca';
+        limiteMaximo = 4;
+        premioelemento = caneca;
+    } else if (angulo > 101 && angulo <= 116.2) {
+        premio = 'patch';
+        limiteMaximo = 95;
+        premioelemento = patch0;
+    } else if (angulo > 116.2 && angulo <= 132.8) {
+        premio = 'abridor';
+        limiteMaximo = 15;
+        premioelemento = abridor0;
+    } else if (angulo > 132.8 && angulo <= 149.7) {
+        premio = 'patch';
+        limiteMaximo = 95;
+        premioelemento = patch1;
+    } else if (angulo > 149.7 && angulo <= 170) {
+        premio = 'suporte';
+        limiteMaximo = 8;
+        premioelemento = suporte;
+    } else if (angulo > 170 && angulo <= 188) {
+        premio = 'sacochila';
+        limiteMaximo = 74;
+        premioelemento = sacochila;
+    } else if (angulo > 188 && angulo <= 208.5) {
+        premio = 'chinelo';
+        limiteMaximo = 20;
+        premioelemento = chinelo1;
+    } else if (angulo > 208.5 && angulo <= 226) {
+        premio = 'boné';
+        limiteMaximo = 32;
+        premioelemento = bone1;
+    } else if (angulo > 226 && angulo <= 244) {
+        premio = 'sacochila';
+        limiteMaximo = 74;
+        premioelemento = sacochila;
+    } else if (angulo > 244 && angulo <= 259.7) {
+        premio = 'patch';
+        limiteMaximo = 95;
+        premioelemento = patch2;
+    } else if (angulo > 259.7 && angulo <= 278.2) {
+        premio = 'sacochila';
+        limiteMaximo = 74;
+        premioelemento = sacochila;
+    } else if (angulo > 278.2 && angulo <= 295.5) {
+        premio = 'patch';
+        limiteMaximo = 95;
+        premioelemento = patch0;
+    } else if (angulo > 295.5 && angulo <= 315.6) {
+        premio = 'abridor';
+        limiteMaximo = 15;
+        premioelemento = abridor1;
+    } else if (angulo > 315.6 && angulo <= 333) {
+        premio = 'patch';
+        limiteMaximo = 95;
+        premioelemento = patch2;
+    } else if (angulo > 333 && angulo <= 353) {
+        premio = 'mousepad';
+        limiteMaximo = 10;
+        premioelemento = mousepad;
+    } else if (angulo > 535 & angulo <= 10.7 ) {
+        premio = 'sacochila';
+        limiteMaximo = 74;
+        premioelemento = sacochila;
+    }
+    if (premio) {
         
-            setTimeout(() =>{
+        if (verificarLimite(premio, limiteMaximo)) {
+            resultado.innerHTML = `O PRÊMIO SORTEADO ${premio === '1' ? 'UM' : premio} JA SE ESGOTOU. VOCÊ PODE GIRAR NOVAMENTE`;
+            playVideo(video1);
+        } else {
+            resultado.innerHTML = `PARABÉNS! VOCÊ GANHOU ${premio === '1' ? 'UM' : premio} `;
             
-            if(valor > 0 && valor <= 22.5){
-                res.innerHTML = 'PARABENS VOCE GANHOU MIL REAIS'    
-            }else if(valor > 22.5 && valor <= 45){
-                res.innerHTML = 'OPSS VOCE PERDEU TUDO'
-                videoplay()
-            }else if( valor > 45 && valor <=67.5){
-                res.innerHTML = 'PARABENS VOCE GANHOU UM REAL'
-                videoplay()
-            }else if(valor > 67.5 && valor <= 90){
-                res.innerHTML = 'VOCE TEM MAIS UMA CHANCE GIRE NOVAMENTE'
-                videoplay()
-            }else if(valor > 90 && valor <= 112.5){
-                res.innerHTML = 'PARABENS VOCE GANHOU QUINHENTOS REAIS'
-                videoplay()
-            }else if(valor > 112.5 && valor <= 135){
-                res.innerHTML = 'O!OU VOCE NÃO GANHOU NADA ACERTOU O ZERO'
-                videoplay()
-            }else if(valor > 135 && valor <= 157.5){
-                res.innerHTML = 'PARABENS VOCE GANHOU DEZ REAIS'
-                videoplay()
-            }else if(valor > 157.5 && valor <= 180){
-                res.innerHTML = 'NÃO FOI DESSA VEZ MAIS NÃO DESISTA'
-                videoplay()
-            }else if(valor > 180 && valor <= 202.8){
-                res.innerHTML = 'VOCE GANHOU DUZENTOS E CINQUENTA REAIS'
-                videoplay()
-            }else if(valor > 202.8 && valor <= 225){
-                res.innerHTML = 'AQUI VOCE PODE DOBAR O VALOR DO SEU PREMIO'
-                videoplay()
-            }else if(valor > 225 && valor <= 247.5){
-                res.innerHTML = 'VOCE GANHOU CEM REAIS'
-                videoplay()
-            }else if(valor > 247.5 && valor <= 270){
-                res.innerHTML = 'QUE LEGAL VOCE PODE GIRAR NOVAMENTE'
-                videoplay()
-            }else if(valor > 270 && valor <= 292.5){
-                res.innerHTML = 'VOCE GANHOU VINTE RREAIS'
-                videoplay()
-            }else if(valor > 292.5 && valor <= 315){
-                res.innerHTML = 'ZERO, VOCE NÃO GANHOU NADA'
-                videoplay()
-            }else if(valor > 315 && valor <= 337.5){
-                res.innerHTML = 'VOCE GANHOU CINCO REAIS'
-                videoplay()
-            }else if(valor > 337.5 && valor <= 360){
-                res.innerHTML = 'OPSS!! VOCE PERDEU TUDO'
-                videoplay()
-            }
-          
+            playVideo(video, premioelemento);
+            adicionarAoEstoque(premio);
             
-        },15500)
+            
+        }
+    } else {
+        const mensagensAlternativas = [
+            { min: 22.5, max: 45, mensagem: 'OPSS! VOCÊ PERDEU TUDO' },
+            { min: 67.5, max: 90, mensagem: 'VOCÊ TEM MAIS UMA CHANCE, GIRE NOVAMENTE' },
+            { min: 112.5, max: 135, mensagem: 'VOCÊ NÃO GANHOU NADA. ACERTOU O ZERO' },
+            { min: 157.5, max: 180, mensagem: 'NÃO FOI DESSA VEZ, MAS NÃO DESISTA' },
+            { min: 202.8, max: 225, mensagem: 'AQUI VOCÊ PODE DOBRAR O VALOR DO SEU PRÊMIO' },
+            { min: 247.5, max: 270, mensagem: 'VOCÊ PODE GIRAR NOVAMENTE' },
+            { min: 292.5, max: 315, mensagem: 'ZERO! VOCÊ NÃO GANHOU NADA' },
+            { min: 337.5, max: 360, mensagem: 'OPSS! VOCÊ PERDEU TUDO' }
+        ];
 
-      
+        const mensagemAlternativa = mensagensAlternativas.find(m => angulo > m.min && angulo <= m.max);
+        if (mensagemAlternativa) {
+            resultado.innerHTML = mensagemAlternativa.mensagem;
+            playVideo(video1);
+        }
     }
-    
-
-    }
-    if(sold.value< 1){
-       sold.value = ''
-    }else{
-        sold.value = sold.value - 1
-    }
-    
-   
 }
 
-
-
-const loadGame = () =>{
-    acumulado.value = 1000
+function verificarLimite(nomePremio, limiteMaximo) {
+    const premios = JSON.parse(localStorage.getItem('meuArr')) || [];
+    const totalPremios = premios.filter(p => p.nome === nomePremio).length;
+    return totalPremios >= limiteMaximo;
 }
 
-loadGame()
+function adicionarAoEstoque(nomePremio) {
+    const premios = JSON.parse(localStorage.getItem('meuArr')) || [];
+    premios.push({ nome: nomePremio });
+    localStorage.setItem('meuArr', JSON.stringify(premios));
+}
+
+function carregarJogo() {
+    acumuladoInput.value = 0;
+    mostreSorteio()
+}
+
+carregarJogo();
  
